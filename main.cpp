41bd8d168c3ed89951824acb0d17ec9557a8f8df
tevzi2 <tevz.beskovnik@gmail.com>
2026-01-05 00:58:49 +0100

    Fix example

--- main.cpp
+++ main.cpp
@@ -1,43 +1,49 @@
 #include "nob.h"
 #include "src/dot_util.h"
+#include <string>
+#include <vector>
+
+using namespace dot;
 
 int main (int argc, char *argv[]) {
     REBUILD_SELF_AND_WATCH(argc, argv, "./src/dot_util.cpp");
 
-    dot::graph_t graph(dot::graph_type_t::digraph, "example");
+    graph_t graph(graph_type_t::digraph, "example");
 
-    dot::node_style_t style;
+    node_style_t style;
     style.color = "black";
     style.fill = "white";
     style.shape = dot::circle;
 
-    dot::node_style_t style_o;
-    style.color = "black";
-    style.fill = "#FF0000";
-    style.shape = dot::circle;
+    node_style_t style_o;
+    style_o.color = "black";
+    style_o.fill = "#FF0000";
+    style_o.shape = dot::circle;
 
-    dot::node_style_t style_i;
-    style.color = "black";
-    style.fill = "#0000FF";
-    style.shape = dot::circle;
+    node_style_t style_i;
+    style_i.color = "black";
+    style_i.fill = "#0000FF";
+    style_i.shape = dot::record;
 
-    dot::node_style_t style_t;
-    style.color = "black";
-    style.fill = "#FFFF00";
-    style.shape = dot::circle;
+    node_style_t style_t;
+    style_t.color = "black";
+    style_t.fill = "#FFFF00";
+    style_t.shape = dot::circle;
 
-    graph.add_node(dot::node_t::create_managed("4", &style));
-    graph.add_node(dot::node_t::create_managed("2", &style_o));
-    graph.add_node(dot::node_t::create_managed("10", &style));
-    graph.add_node(dot::node_t::create_managed("6", &style_t));
-    graph.add_node(dot::node_t::create_managed("8", &style_t));
-    graph.add_node(dot::node_t::create_managed("22", &style));
-    graph.add_node(dot::node_t::create_managed("1", &style_i));
+    std::vector<std::string> data = { "5", "7", "1" };
 
-    graph.add_connection("1", "4");
-    graph.add_connection("1", "10");
-    graph.add_connection("1", "2");
-    graph.add_connection("1", "22");
+    graph.add_node(node_t::create_managed("4", &style));
+    graph.add_node(node_t::create_managed("2", &style_o));
+    graph.add_node(node_t::create_managed("10", &style));
+    graph.add_node(node_t::create_managed("6", &style_t));
+    graph.add_node(node_t::create_managed("8", &style_t));
+    graph.add_node(node_t::create_managed("22", &style));
+    graph.add_node(multy_node_t::create_managed("1", data, &style_i));
+
+    graph.add_connection("\"1\":f0", "4");
+    graph.add_connection("\"1\":f1", "10");
+    graph.add_connection("\"1\":f2", "2");
+    graph.add_connection("\"1\":f3", "22");
     graph.add_connection("4", "6");
     graph.add_connection("8", "1");
     graph.add_connection("22", "8");
